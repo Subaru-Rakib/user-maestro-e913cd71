f69a5228554b2c976938c42b3c52f456368a9482
Subaru-Rakib <mdrakibhasan6577@gmail.com>
2026-09-04 12:58:48 +0600

    Fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
+import { defineConfig } from "@lovable.dev/vite-tanstack-config";
+
 export default defineConfig({
   vite: {
     base: "./",
   },
+
   tanstackStart: {
     server: { entry: "server" },
   },
