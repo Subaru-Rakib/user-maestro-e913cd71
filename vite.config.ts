819f0d334a2c25fcf2020bc350882600531a59d1
Subaru-Rakib <mdrakibhasan6577@gmail.com>
2026-09-04 12:48:35 +0600

    Fix base for Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,3 @@
-// @lovable.dev/vite-tanstack-config already includes the following — do NOT add them manually
-import { defineConfig } from "@lovable.dev/vite-tanstack-config";
-
 export default defineConfig({
   vite: {
     base: "./",
